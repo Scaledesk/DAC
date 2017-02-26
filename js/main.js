29c0fd76a366270f408a0777748864ef4ddf9c71
sanchitrathee <sanchit2411@gmail.com>
2017-02-26 20:53:44 +0530

    new nav bar with drop down and about us baner

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
+$(document).ready(function(){
+  
 
+$('#menu1 , .dropdown-menu').hover(function() {
+  $('ul.nav').find('.dropdown-menu').stop(true, true).delay(200).fadeIn(10);
+}, function() {
+  $('ul.nav').find('.dropdown-menu').stop(true, true).delay(200).fadeOut(0);
+});
+    
+    
+    });
