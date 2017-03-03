49f553198a101c5b22d0944f2ecc7c3fdc263a66
garima <garima8jun@gmail.com>
2017-03-03 18:12:05 +0530

    fixed div on scroll

--- js/main.js
+++ js/main.js
@@ -6,6 +6,23 @@ $('#menu1 , .dropdown-menu').hover(function() {
 }, function() {
   $('ul.nav').find('.dropdown-menu').stop(true, true).delay(200).fadeOut(0);
 });
-    
-    
+
     });
+
+
+
+
+$(document).ready(function() {
+    $(window).scroll(function() {
+
+        var headerH = $('.slider-sec').outerHeight(true);
+        console.log(headerH);
+//this will calculate header's full height, with borders, margins, paddings
+        var scrollVal = $(this).scrollTop();
+        if ( scrollVal > headerH ) {
+            $('#fb').css({'position':'fixed','top' :'95px','background-color':'#f68f8a','width':'168vh'});
+        } else {
+            $('#fb').css({'position':'static','top':'0px','width':'168vh'});
+        }
+    });
+});
